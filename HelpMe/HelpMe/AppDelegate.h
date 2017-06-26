043a1eee789dd22acf6fc9a9c2aaa18f643b9d20
kevin Cleathero <kengc@hotmail.com>
2017-06-26 11:14:15 -0700

    project setup

---
+++ HelpMe/HelpMe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelpMe
+//
+//  Created by Kevin Cleathero on 2017-06-26.
+//  Copyright © 2017 Kevin Cleathero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelpMe/HelpMe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelpMe
+//
+//  Created by Kevin Cleathero on 2017-06-26.
+//  Copyright © 2017 Kevin Cleathero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
