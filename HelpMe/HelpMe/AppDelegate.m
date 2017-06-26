60550bf99f8bed5f8cd97f5b4daef7007da85bf2
kevin Cleathero <kengc@hotmail.com>
2017-06-26 14:39:08 -0700

    added basic Firebase

--- HelpMe/HelpMe/AppDelegate.m
+++ HelpMe/HelpMe/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <Firebase/Firebase.h>
 
 @interface AppDelegate ()
 
@@ -16,6 +17,8 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [FIRApp configure];
+    
     // Override point for customization after application launch.
     return YES;
 }
